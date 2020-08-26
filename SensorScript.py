e9902b20cb079e9733103836b62297499ccdc64a
funkyxive <andreasnordenhof@gmail.com>
2020-08-26 09:05:30 +0200

    updated the sensorscript to only send data when there's a change in temperature in a certain time of day as well as first in the morning

--- SensorScript.py
+++ SensorScript.py
@@ -56,6 +56,7 @@ try:  # try except so we restart the raspberry pi if the program crashes
         else:
             print("not posting", initialCheck)
             initialCheck = True #sets initial check to true so we post data on first check in the specified time frame
+            time.sleep(10)
 except (KeyboardInterrupt, SystemExit):  # makes it so the pi doesn't restart at the exceptions specified
     raise
 except:  # restarts the raspberry pi on all other exceptions
