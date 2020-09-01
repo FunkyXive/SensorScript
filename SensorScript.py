d686aa27b3943398116565094315d7861618a61b
funkyxive <andreasnordenhof@gmail.com>
2020-09-01 11:14:35 +0200

    added static checks at 8 12 and 15

--- SensorScript.py
+++ SensorScript.py
@@ -26,22 +26,41 @@ zone = int(hostname[1])  # stores the second entry in the list as an int
 url = "http://infotavle.itd-skp.sde.dk/TH_API/ClimateSensor_Api/api/climateSensor/create.php"  # url of our api
 pin = 4  # the io pin on our raspberry pi that is connected to the data pin on the DHT22
 lastCheck = time.time()  # set's the initial last check time
-sensorStartHour = 6 #the hour to start sensors
-sensorEndHour = 18 #the end for the sensors
+sensorStartHour = 6  # the hour to start sensors
+sensorEndHour = 18  # the end for the sensors
 initialCheck = True  # sets initial-check to true so we get first reading immediately
 sensor = DHT.DHT22  # defines which sensor of the supported sensors that we are using
 temperatureDeviance = 0.5  # variable controlling how much the temperature needs to change each check
-lastHum, lastTemp = 1, 1 #random base value for last check that makes the sensor always send when started up
+lastHum, lastTemp = 1, 1  # random base value for last check that makes the sensor always send when started up
+
+
+def staticCheck(currentTime, checkHour, checkMinute):
+    h, t = DHT.read_retry(sensor,
+                          pin)  # reads humidity and temperature from sensor, retries up to 15 times if it fails
+    if currentTime.hour == checkHour and currentTime.minute == checkMinute:
+        print(f"Temperature: {t}*C, Humidity: {h}%")  # prints the data for testing and monitoring purposes
+        r = requests.post(url, json={"ipaddress": ip, "zone": zone, "name": name,
+                                     "updated": str(datetime.datetime.now()),
+                                     "temperature": t,
+                                     "humidity": h})  # posting the data as json to our api via the url
+        print(r.status_code)  # prints the status code of the post request 201 for success
+        print(f"posted static {checkHour}")
+        if r.status_code == 201:
+            time.sleep(61)
 try:  # try except so we restart the raspberry pi if the program crashes
     while True:  # main data loop
         currentTime = datetime.datetime.now()  # gets the current time
         hour = currentTime.hour  # gets the current hour 0-23
-        if sensorStartHour <= hour <= sensorEndHour: #checks if current hour is between start hour and end hour, if true, run hour script, if not don't
+        staticCheck(currentTime, 8, 0)
+        staticCheck(currentTime, 12, 0)
+        staticCheck(currentTime, 15, 0)
+        if sensorStartHour <= hour <= sensorEndHour:  # checks if current hour is between start hour and end hour, if true, run hour script, if not don't
             if time.time() - lastCheck >= 60 or initialCheck:  # checks if 1(60seconds) minutes has passed since last check or if this is the first check since we started the program
                 lastCheck = time.time()  # updates the lastCheck variable to the new time
                 h, t = DHT.read_retry(sensor,
                                       pin)  # reads humidity and temperature from sensor, retries up to 15 times if it fails
-                if abs(t - lastTemp) > temperatureDeviance or initialCheck:  # checks if the difference between the current temp and last temp is more than the set deviance threshold, if it is then it sends data, if not it prints the temp difference
+                if abs(
+                        t - lastTemp) > temperatureDeviance or initialCheck:  # checks if the difference between the current temp and last temp is more than the set deviance threshold, if it is then it sends data, if not it prints the temp difference
                     initialCheck = False  # sets initialCheck to false so we don't spam the server
                     print(f"Temperature: {t}*C, Humidity: {h}%")  # prints the data for testing and monitoring purposes
                     r = requests.post(url, json={"ipaddress": ip, "zone": zone, "name": name,
@@ -49,14 +68,15 @@ try:  # try except so we restart the raspberry pi if the program crashes
                                                  "temperature": t,
                                                  "humidity": h})  # posting the data as json to our api via the url
                     print(r.status_code)  # prints the status code of the post request 201 for success
-                    lastHum, lastTemp = h, t #sets the last check temp and hum
+                    lastHum, lastTemp = h, t  # sets the last check temp and hum
                     print("posted")
                 else:
-                    print(abs(t - lastTemp), f"temp, lastTemp: {t}, {lastTemp}")#prints the deviance
+                    print(abs(t - lastTemp), f"temp, lastTemp: {t}, {lastTemp}")  # prints the deviance
         else:
             print(f"not posting{sensorStartHour}{hour}{sensorEndHour}")
-            initialCheck = True #sets initial check to true so we post data on first check in the specified time frame
+            initialCheck = True  # sets initial check to true so we post data on first check in the specified time frame
             time.sleep(10)
+
 except (KeyboardInterrupt, SystemExit):  # makes it so the pi doesn't restart at the exceptions specified
     raise
 except:  # restarts the raspberry pi on all other exceptions
