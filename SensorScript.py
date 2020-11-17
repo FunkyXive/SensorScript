e462d037f2e14f8a37039f85c7cde78370831e21
funkyxive <andreasnordenhof@gmail.com>
2020-11-17 12:39:06 +0100

    updated static check function

--- SensorScript.py
+++ SensorScript.py
@@ -79,5 +79,8 @@ try:  # try except so we restart the raspberry pi if the program crashes
 
 except (KeyboardInterrupt, SystemExit):  # makes it so the pi doesn't restart at the exceptions specified
     raise
-except:  # restarts the raspberry pi on all other exceptions
+except Exception as e:  # restarts the raspberry pi on all other exceptions
+    r = requests.post(url, json={"Error": e, "zone": zone, "name": name})
+    print(r.status_code)
+    print("posted")
     os.system('sudo reboot')
